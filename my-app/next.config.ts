ede84d311b798701ff4c23b11e9fc1e931305568
Bryan <bg478806@gmail.com>
2025-11-27 17:22:35 -0500

    added netlify.toml file inorder to bypass autodetect from netlify which has a known bug where base + deplayment dir becomes locked

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -2,9 +2,12 @@ import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  serverExternalPackages: [], // instead of experimental.serverComponentsExternalPackages
-  // Remove experimental.appDir entirely — appDir is now stable
-  turbopack: {}, // empty object silences the Turbopack warning if you have a webpack config
+
+  
+  output: 'export',
+
+  serverExternalPackages: [],
+  turbopack: {},
 };
 
 export default nextConfig;
